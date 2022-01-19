1f6aaa957c82a5cee30eed8328efe92c8fb239c4
Jaewon Choi <jaewon.james.choi@gmail.com>
2022-01-19 19:51:02 +0900

    Initial commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Jaewon Choi on 2022/01/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/atApp.swift
@@ -0,0 +1,17 @@
+//
+//  atApp.swift
+//  Shared
+//
+//  Created by Jaewon Choi on 2022/01/19.
+//
+
+import SwiftUI
+
+@main
+struct atApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
